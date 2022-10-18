0cab520f2ad757a7ee3c5f41ef7ce2995045cace
Jessika-prog <jessikahouete@orange.fr>
2022-10-18 20:23:05 +0200

    amélioration front

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,11 @@
 
-
 // Animation accueil
 const banniere = document.querySelector('.banniere')
 const slider = document.querySelector('.background')
 const logo = document.querySelector('#logo')
 const titre = document.querySelector('.titre')
 const partie = document.querySelectorAll('.partie')
+
 
 
 gsap.fromTo(banniere,1, {height: "0%"}, {height:"80%",ease: Power2.easeInOut})
@@ -16,5 +16,19 @@ gsap.from(partie, 1, {opacity:0, ease: "power1", stagger:{
   each: 0.14,
   from: "center"
 }});
+
+
+
+
+let overlay1 = document.querySelector('.overlay1')
+let overlay2 = document.querySelector('.overlay2')
+let overlay3 = document.querySelector('.overlay3')
+let overlay4 = document.querySelectorAll('.overlay4')
+
+gsap.fromTo(overlay1, 7, {skewX: 30, scale: 1.5}, {skewX: 0, xPercent:100, transformOrigin: "0% 100%", ease: Power4.easeOut, delay:3})
+gsap.fromTo(overlay2, 5, {skewX: 30, scale: 1.5}, {skewX: 0, xPercent:100, transformOrigin: "0% 100%", ease: Power4.easeOut, delay:8})
+gsap.fromTo(overlay3, 5, {skewX: 30, scale: 1.5}, {skewX: 0, xPercent:100, transformOrigin: "0% 100%", ease: Power4.easeOut, delay:12})
+gsap.fromTo(overlay4, 5, {skewX: 30, scale: 1.5}, {skewX: 0, xPercent:100, transformOrigin: "0% 100%", ease: Power4.easeOut, delay:17})
+
 
 
